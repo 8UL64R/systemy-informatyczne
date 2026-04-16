d8f11bc61df1719433b54742e229111e6060ee20
Jakub Kiermaszek <kuba.kiermaszek@op.pl>
2026-04-16 14:38:47 +0200

    cococococo

---
+++ JakisProjekt/trzeci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JakisProjekt
+{
+    public class trzeci
+    {
+        public string napis = "To jest trzeci branch";
+        
+        public void Wypisz()
+        {
+            Console.WriteLine(napis + ";)");
+        }
+    }
+}
