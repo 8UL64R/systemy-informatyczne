1d107791a51b9721ee5e85c58f5975a8436483a6
student <student@RIB1F-L012-S21>
2026-04-16 14:46:37 +0200

    dodanie funkcjihngjbnjnbtjnbjo;jnll

--- JakisProjekt/Program.cs
+++ JakisProjekt/Program.cs
@@ -3,3 +3,9 @@
 int a = 2;
 int b = 4;
 int wynik = a * b;
+
+int wynik1 = a / b;
+
+Console.WriteLine(wynik);
+Console.WriteLine(wynik1);
+int c;
--- JakisProjekt/obj/Release/net8.0/JakisProjekt.AssemblyInfo.cs
+++ JakisProjekt/obj/Release/net8.0/JakisProjekt.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("JakisProjekt")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab9a080022145774d639841a7251024297919fb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("JakisProjekt")]
